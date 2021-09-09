f96e809a69a1967b1261511c8016f5c2747bf717
Gleiston Machado <gleiston.dev@gmail.com>
2021-09-09 19:26:50 -0300

    Projetos criados

---
+++ backend/src/test/java/com/gleiston/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleiston.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
