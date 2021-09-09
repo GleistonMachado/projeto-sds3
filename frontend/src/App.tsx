78dca533ade3b03e1dc3ca4f0276975ec427210b
Gleiston Machado <gleiston.dev@gmail.com>
2021-09-09 20:55:57 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import BarChart from 'components/BarChart';
+import DanutChart from 'components/DanutChart';
 import DataTable from 'components/DataTable';
 import Footer from 'components/Footer';
 import NavBar from 'components/NavBar';
@@ -8,7 +10,24 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Vendas</h5>
+            <DanutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Lista de Vendas</h2>
+        </div>
+
         <DataTable />
       </div>
       <Footer />
